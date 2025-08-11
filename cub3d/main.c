e2b2252b6058bb99f319bc6b1b7d8e347099f8c7
ilorez <zobirnajdaoui@gmail.com>
2025-08-11 13:29:13 +0100

    cancel moving map view, to later

--- cub3d/main.c
+++ cub3d/main.c
@@ -6,11 +6,12 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 15:57:11 by znajdaou          #+#    #+#             */
-/*   Updated: 2025/08/11 11:20:50 by znajdaou         ###   ########.fr       */
+/*   Updated: 2025/08/11 13:28:16 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "./includes/container.h"
+#include "debug/debug.h"
 
 int main()
 {
@@ -19,7 +20,7 @@ int main()
   // get map
 
   ;
-  data.map = generate_map(200, 200);
+  data.map = example_map();
   ft_setup(&data);
   //render_map(data.map);
   //render_player(&data);
