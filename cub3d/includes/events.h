bce314d4fb80ee80712dcf817e451e4a68b93af8
ilorez <zobirnajdaoui@gmail.com>
2025-08-16 10:05:04 +0100

    working on norminette

--- cub3d/includes/events.h
+++ cub3d/includes/events.h
@@ -14,7 +14,7 @@
 # define EVENTS_H
 
 // include
-#include "./container.h"
+# include "./container.h"
 
 // functions
 
@@ -25,12 +25,12 @@ int	ft_destroy_event(t_data *data);
 int	ft_loop_hook(t_data *data);
 
 // key hook
-int ft_key_press(int keycode, t_data *data);
-int ft_key_release(int keycode, t_data *data);
+int	ft_key_press(int keycode, t_data *data);
+int	ft_key_release(int keycode, t_data *data);
 
 // mouse click
 int	ft_mouse_click(int key, int x, int y, t_data *data);
-int on_mouse_move(int x, int y, t_data *d);
-int on_mouse_enter(t_data *data);
+int	on_mouse_move(int x, int y, t_data *d);
+int	on_mouse_enter(t_data *data);
 
 #endif
