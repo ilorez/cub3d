b6ac794fd759125c4ec28c6b97756a0d5ce2f0ce
ilorez <zobirnajdaoui@gmail.com>
2025-08-16 11:10:10 +0100

    normitte srcs/events

--- cub3d/srcs/events/loop.c
+++ cub3d/srcs/events/loop.c
@@ -6,53 +6,66 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 14:06:20 by znajdaou          #+#    #+#             */
-/*   Updated: 2025/08/16 09:47:03 by znajdaou         ###   ########.fr       */
+/*   Updated: 2025/08/16 11:02:24 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../../includes/events.h"
+#include "../../includes/events.h"
 #include <stdlib.h>
 
-time_t	ft_time_now(void);
+time_t			ft_time_now(void);
+// render window components
+static void		draw(t_data *data);
+static time_t	calculate_delta_time(t_data *data);
+static void		count_fbs(t_data *data, time_t current_t);
 
 int	ft_loop_hook(t_data *data)
 {
-  time_t current_t;
+	time_t	current_t;
 
-  // calculate delta time
-  if (!data)
-		  return (1);
-  current_t = ft_time_now();
-  if (data->lastf == 0)
-    data->lastf = current_t;
-  data->delta_time = (current_t -  data->lastf) / 1000.0f;
-  data->lastf = current_t;
-	
-  // render window components 
-  render_map(data, data->map);
-  //ft_bzero(data->img.addr, WIN_WIDTH * WIN_HEIGHT * (data->img.bpp / 8));
-  render_fc(data);
-  raycast(data);
-  render_player(data);
-  render_aim(data->img, WIN_WIDTH/2, WIN_HEIGHT/2);
+	if (!data)
+		return (1);
+	current_t = calculate_delta_time(data);
+	draw(data);
+	count_fbs(data, current_t);
+	if (!data->is_running)
+		ft_handle_window_exit(data, EXIT_SUCCESS);
+	return (0);
+}
 
-  //mlx_string_put(data->mlx, data->win, WIN_WIDTH - 100, 20, 0xFFFFFF, "FPS:");
-  mlx_put_image_to_window(data->mlx, data->win, data->img.img, 0, 0);
-  mlx_put_image_to_window(data->mlx, data->win, data->map->data.img, 0, 0);
+static void	draw(t_data *data)
+{
+	render_map(data, data->map);
+	render_fc(data);
+	raycast(data);
+	render_player(data);
+	render_aim(data->img, WIN_WIDTH / 2, WIN_HEIGHT / 2);
+	mlx_put_image_to_window(data->mlx, data->win, data->img.img, 0, 0);
+	mlx_put_image_to_window(data->mlx, data->win, data->map->data.img, 0, 0);
+}
 
-  // count fbs
-  data->frame_count++;
-	if (current_t - data->last_fps_time >= 1000.0) // every 1 second
+static time_t	calculate_delta_time(t_data *data)
+{
+	time_t	current_t;
+
+	current_t = ft_time_now();
+	if (data->lastf == 0)
+		data->lastf = current_t;
+	data->delta_time = (current_t - data->lastf) / 1000.0f;
+	data->lastf = current_t;
+	return (current_t);
+}
+
+static void	count_fbs(t_data *data, time_t current_t)
+{
+	data->frame_count++;
+	if (current_t - data->last_fps_time >= 1000.0)
 	{
 		printf("FPS: %d\n", data->frame_count);
 		data->frame_count = 0;
 		data->last_fps_time = current_t;
 	}
-  if (!data->is_running)
-    ft_handle_window_exit(data, EXIT_SUCCESS);
-	return (0);
 }
-
 
 time_t	ft_time_now(void)
 {
--- cub3d/srcs/events/mouse.c
+++ cub3d/srcs/events/mouse.c
@@ -1,50 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mouse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/16 10:47:31 by znajdaou          #+#    #+#             */
+/*   Updated: 2025/08/16 10:55:24 by znajdaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../../includes/container.h"
-#include <stdio.h>
 
-int	ft_mouse_click(int key, int x, int y, t_data *data)
+// #include <printf.h>
+// int	ft_mouse_click(int key, int x, int y, t_data *data)
+//{
+//	(void)x, (void)y, (void)data;
+//	if (key == KEY_VIEW_LEFT)
+//		printf("left\n");
+//	else if (key == KEY_VIEW_RIGHT)
+//		printf("right\n");
+//	return (0);
+//}
+
+int	on_mouse_enter(t_data *data)
 {
-  (void) x, (void) y , (void) data;
-	if (key == KEY_VIEW_LEFT)
-      printf("left\n");
-  else if (key == KEY_VIEW_RIGHT)
-      printf("right\n");
+	if (!(data->mouse.lock))
+		lock_mouse(data);
 	return (0);
 }
 
-int on_mouse_enter(t_data *data)
+static int	limit_d(int d, int min, int max, int deadzone)
 {
-  if (!(data->mouse.lock))
-    lock_mouse(data);
-  return (0);
+	if (d < min)
+		d = min;
+	if (d > max)
+		d = max;
+	if (d <= deadzone && d >= -1 * deadzone)
+		d = 0;
+	return (d);
 }
 
-int on_mouse_move(int x, int y, t_data *d)
+int	on_mouse_move(int x, int y, t_data *d)
 {
-  int dx, dy;
-  if (!d->mouse.lock)
-    return (0);
-  if (d->mouse.ignore_next_move)
-  {
-    d->mouse.ignore_next_move = 0;
-    return (0);
-  }
-  //printf("mouse has moved to : x=>%d,y=>%d\n", x, y);
-  dx = x - WIN_WIDTH/2;
-  dy = y - WIN_HEIGHT/2;
-  if (dx < -WIN_WIDTH/2) dx = -WIN_WIDTH/2;
-  if (dx > WIN_WIDTH/2) dx = WIN_WIDTH/2;
-  if (dy < -WIN_HEIGHT/2) dy = -WIN_HEIGHT/2;
-  if (dy > WIN_HEIGHT/2) dy = WIN_HEIGHT/2;
-  printf("%d\n", dx);
-  // ignore move less then 1 by using int type
-  if (dx <= 1 && dx >= -1) dx = 0;
-  if (dy <= 1 && dy >= -1) dy = 0;
-  d->mouse.dx_accum += dx; 
-  d->mouse.dy_accum += dy; 
-  mlx_mouse_move(d->mlx, d->win, WIN_WIDTH/2, WIN_HEIGHT/2);
-  d->mouse.ignore_next_move = 1;
-  return (0);
+	int	dx;
+	int	dy;
+
+	if (!d->mouse.lock)
+		return (0);
+	if (d->mouse.ignore_next_move)
+	{
+		d->mouse.ignore_next_move = 0;
+		return (0);
+	}
+	dx = x - WIN_WIDTH / 2;
+	dy = y - WIN_HEIGHT / 2;
+	dx = limit_d(dx, -WIN_WIDTH / 2, WIN_WIDTH / 2, 1);
+	dy = limit_d(dy, -WIN_HEIGHT / 2, WIN_HEIGHT / 2, 1);
+	d->mouse.dx_accum += dx;
+	d->mouse.dy_accum += dy;
+	mlx_mouse_move(d->mlx, d->win, WIN_WIDTH / 2, WIN_HEIGHT / 2);
+	d->mouse.ignore_next_move = 1;
+	return (0);
 }
-
-
