35bc828e6a0af52ff13a2e541353fd2c286e33a4
ilorez <zobirnajdaoui@gmail.com>
2025-09-02 11:13:05 +0100

    order mean commands

--- cub3d/main.c
+++ cub3d/main.c
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 15:57:11 by znajdaou          #+#    #+#             */
-/*   Updated: 2025/09/02 11:01:32 by znajdaou         ###   ########.fr       */
+/*   Updated: 2025/09/02 11:10:34 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,21 @@
 
 int main(int ac,char **av)
 {
-  (void)ac;
   t_cub_data pars_data;
+  t_data data;
 
+  (void)ac;
   /* bonus parsing test*/
   ft_memset(&pars_data, 0, sizeof(pars_data));
   if(parse_file_path(av[1], &pars_data))
     print_cub_data(&pars_data);
   convert_char_arr_to_int_grid(&pars_data.map);
-
   // TODO
   //free_cub_data(&data);
-  (void)ac, (void)av;
+  
   // example data
-  //t_cub_data *info;
-  //info  = example_usage2();
+  //pars_data = *example_usage2();
   // raycasting
-  t_data data;
-  data.last_fps_time = 0;
   ft_setup(&data, &pars_data);
 	mlx_loop(data.mlx);
 }
